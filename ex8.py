88864958beb5797e6ef4c584bc8f22b54342be5d
Karina <32357095+s189994@users.noreply.github.com>
2017-10-19 15:50:02 +0100

    Update ex8.py

--- ex8.py
+++ ex8.py
@@ -1,23 +1,21 @@
-total_cost = float(input("Total cost of the house"))
 portion_deposit = 0.20
 current_savings = 0
 r = 0.04
-current_savings = current_savings*r/12
-
-annual_salary= float(input("Enter the starting annual salary:"))
-portion_saved= float(input("How much money do you want to save?"))
-semi_annual_raise=float(input("What is your semi-annual raise (decimal): "))
+monthly_interest = current_savings*(r/12)
 monthly_salary= annual_salary/12
 
-deposit_payment = total_cost*portion_deposit
+total_cost = float(input("Total cost of the house"))
+annual_salary= float(input("Enter the starting annual salary:"))
+portion_saved= float(input("How much money do you want to save?"))
+semi_annual_raise=float(input("By what proportion does you salary increase every six months?"))
+
+portion_deposit = total_cost*portion_deposit
 months = 0
 
 while current_savings < total_cost*portion_deposit:
-    current_savings += (current_savings*(r/12))
-    current_savings +=(annual_salary/12) * portion_saved
+    current_savings += current_savings*(r/12)
+    current_savings += monthly_salary * portion_saved
     months += 1
-    if months %6 == 0:
-        annual_salary*= (1+ semi_annual_raise)
-
+  monthly_salary += monthly_salary * (semi_annual_raise/12)
 
 print("Number of months:", months)
